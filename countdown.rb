f78218cf53d51c9ba81f2a24db7f3b3a64ccac71
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2019-10-11 16:18:15 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,7 +8,7 @@ def countdown(seconds)
   return "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep
+def countdown_with_sleep(sleep.5s)
   while seconds > 0 
       puts "#{seconds} SECOND(S)!"
   seconds -= 1
